942b6a9e037e7b9ac0c3022e1695304aa2ee452b
Monika Paulin <81420553+MonikaPaulin@users.noreply.github.com>
2021-03-29 18:44:44 +0530

    Update codecheckout.groovy

--- vars/codecheckout.groovy
+++ vars/codecheckout.groovy
@@ -1,5 +1,3 @@
 def call(){
-checkout(
-[$class: 'GitSCM', branches: [[name: '*/main']], userRemoteConfigs: [[credentialsId: 'cafd918a-206d-4a9d-8165-5bc14ec467a8', url: 'https://github.com/MonikaPaulin/Release-Report-Jenkins-Pipeline.git']
-]])
+checkout([$class: 'GitSCM', branches: [[name: '*/main']], userRemoteConfigs: [[credentialsId: 'cafd918a-206d-4a9d-8165-5bc14ec467a8', url: 'https://github.com/MonikaPaulin/Release-Report-Jenkins-Pipeline.git']]])
 }
