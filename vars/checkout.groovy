52e809ca760e78295632f281f2142c01becd4abc
Monika Paulin <81420553+MonikaPaulin@users.noreply.github.com>
2021-03-29 18:19:46 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,7 +1,7 @@
 def call(){
 checkout(
 [$class: 'GitSCM', 
-branches: [[name: 'main']], 
+branches: [[name: '*/main']], 
 extensions: [], 
 userRemoteConfigs: [
 [credentialsId: 'cafd918a-206d-4a9d-8165-5bc14ec467a8', 
