39d9187f7366c1a5ea298e41d40942b2fa2bfcb4
Monika Paulin <81420553+MonikaPaulin@users.noreply.github.com>
2021-03-29 21:01:20 +0530

    Update emailnotify.groovy

--- vars/emailnotify.groovy
+++ vars/emailnotify.groovy
@@ -1,5 +1,5 @@
 void call() {
 emailext attachLog: true, attachmentsPattern: 'Release_Status.csv'
-body: 'Attached is the release report log file.', 
+body: "Attached is the release report log file.", 
 replyTo: 'MonikaPaulin.Jeyakumar@cognizant.com', subject: 'Release Report Jenkins Pipeline', to: 'MonikaPaulin.Jeyakumar@cognizant.com'
 }
