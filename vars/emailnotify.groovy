2c621a36d419407d10846ad74c485fe0a57513e5
Monika Paulin <81420553+MonikaPaulin@users.noreply.github.com>
2021-03-30 13:34:08 +0530

    Update emailnotify.groovy

--- vars/emailnotify.groovy
+++ vars/emailnotify.groovy
@@ -1,6 +1,6 @@
 void call() {
 emailext(       attachLog: true, attachmentsPattern: 'Release_Status.csv',
-                subject: "${currentBuild.result}: ${env.JOB_NAME} [${env.BUILD_NUMBER}]",
+                subject: "${env.JOB_NAME}: [${env.BUILD_NUMBER}]",
                 to: 'monikalevale@gmail.com',               
                 body: "PFA"
             )
